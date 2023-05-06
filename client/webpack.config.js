b13ce7a88754c7f2df13c4ef791db51b6216e721
michaeloc1 <michaeloc@verizon.net>
2023-05-06 16:41:20 -0400

    added manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Edit anything you want',
+        background_color: '#FFD700',
+        theme_color: '#FFD700',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
